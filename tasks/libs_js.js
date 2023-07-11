51f2bafd6c02efa359d1ca56428832d11bd70405
viktor.egai <egai.viktor94@gmail.com>
2023-07-11 15:25:33 +0300

    first commit

---
+++ tasks/libs_js.js
@@ -0,0 +1,29 @@
+const plugins = [
+	'node_modules/@fancyapps/ui/dist/fancybox/fancybox.umd.js',
+	'node_modules/swiper/swiper-bundle.min.js',
+	// 'node_modules/gsap/dist/gsap.min.js',
+	// 'node_modules/gsap/dist/ScrollTrigger.min.js',
+	'node_modules/inputmask/dist/inputmask.min.js',
+];
+const {
+	src,
+	dest
+} = require('gulp');
+const uglify = require('gulp-uglify-es').default;
+const concat = require('gulp-concat');
+const map = require('gulp-sourcemaps');
+const chalk = require('chalk');
+
+module.exports = function libs_js(done) {
+	if (plugins.length > 0)
+		return src(plugins)
+			.pipe(map.init())
+			.pipe(uglify())
+			.pipe(concat('libs.min.js'))
+			.pipe(map.write('../sourcemaps'))
+			.pipe(dest('build/js/'))
+			.pipe(dest('theme/js/'))
+	else {
+		return done(console.log(chalk.redBright('No added JS plugins')));
+	}
+}
---
+++ tasks/libs_style.js
@@ -0,0 +1,30 @@
+const plugins = [		
+	'node_modules/@fancyapps/ui/dist/fancybox/fancybox.css',
+	'node_modules/swiper/swiper-bundle.min.css',
+	'node_modules/bootstrap/dist/css/bootstrap-grid.css',
+];
+
+const {
+	src,
+	dest
+} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const map = require('gulp-sourcemaps');
+const chalk = require('chalk');
+
+module.exports = function libs_style(done) {
+	if (plugins.length > 0) {
+		return src(plugins)
+			.pipe(map.init())
+			.pipe(sass({
+				outputStyle: 'compressed'
+			}).on('error', sass.logError))
+			.pipe(concat('libs.min.css'))
+			.pipe(map.write('../sourcemaps/'))
+			.pipe(dest('build/css/'))
+			.pipe(dest('theme/css/'))
+	} else {
+		return done(console.log(chalk.redBright('No added CSS/SCSS plugins')));
+	}
+}
