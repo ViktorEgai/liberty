f3dcfc1cae807f545273764003b12eba2044c6dd
viktor.egai <egai.viktor94@gmail.com>
2023-07-17 19:49:07 +0300

    added gulp version plugin

--- tasks/html.js
+++ tasks/html.js
@@ -4,10 +4,20 @@ const {
 } = require('gulp');
 const include = require('gulp-file-include');
 const bs = require('browser-sync');
+const version = require('gulp-version-number');
+const versionConfig = {
+  'value': '%MDS%',
+  'append': {
+    'key': 'v',
+    'to': ['css', 'js'],
+  },
+};
+
 
 module.exports = function html() {
 	return src(['src/**/*.html', '!src/components/**/*.html'])
 		.pipe(include())
+		.pipe(version(versionConfig))
 		.pipe(dest('build'))
 		.pipe(bs.stream())
 }
