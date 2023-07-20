4b7a0f8e24d550ceeee08da932d12d04a134082b
viktor.egai <egai.viktor94@gmail.com>
2023-07-20 18:13:29 +0300

    added objects.html

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,6 +1,59 @@
 document.addEventListener("DOMContentLoaded", () => {
 	Fancybox.bind("[data-fancybox]");
 
+			class ItcCollapse {
+			constructor(target, duration = 350) {
+				this._target = target;
+				this._duration = duration;
+			}
+			show() {
+				const el = this._target;
+				if (el.classList.contains("collapsing") || el.classList.contains("collapse_show")) {
+					return;
+				}
+				el.classList.remove("collapse");
+				const height = el.offsetHeight;
+				el.style.height = 0;
+				el.style.overflow = "hidden";
+				el.style.transition = `height ${this._duration}ms ease`;
+				el.classList.add("collapsing");
+				el.offsetHeight;
+				el.style.height = `${height}px`;
+				window.setTimeout(() => {
+					el.classList.remove("collapsing");
+					el.classList.add("collapse");
+					el.classList.add("collapse_show");
+					el.style.height = "";
+					el.style.transition = "";
+					el.style.overflow = "";
+				}, this._duration);
+			}
+			hide() {
+				const el = this._target;
+				if (el.classList.contains("collapsing") || !el.classList.contains("collapse_show")) {
+					return;
+				}
+				el.style.height = `${el.offsetHeight}px`;
+				el.offsetHeight;
+				el.style.height = 0;
+				el.style.overflow = "hidden";
+				el.style.transition = `height ${this._duration}ms ease`;
+				el.classList.remove("collapse");
+				el.classList.remove("collapse_show");
+				el.classList.add("collapsing");
+				window.setTimeout(() => {
+					el.classList.remove("collapsing");
+					el.classList.add("collapse");
+					el.style.height = "";
+					el.style.transition = "";
+					el.style.overflow = "";
+				}, this._duration);
+			}
+			toggle() {
+				this._target.classList.contains("collapse_show") ? this.hide() : this.show();
+			}
+		}
+
 	const toggleMobileMenu = () => {
 		const btn = document.querySelector(".header-menu-btn"),
 			menu = document.querySelector(".header-nav"),
@@ -336,7 +389,6 @@ document.addEventListener("DOMContentLoaded", () => {
 			});
 		}
 	};
-
 	partnersSlider();
 
 	// news swiper
@@ -504,9 +556,9 @@ document.addEventListener("DOMContentLoaded", () => {
 			
 		}
 	};
-
 	teamSlider();
 
+	// Показать больше
 	const showMoreText = () => {
 		const textBlock = document.querySelectorAll(".text-block");
 		if (textBlock !== null) {
@@ -549,7 +601,21 @@ document.addEventListener("DOMContentLoaded", () => {
 		})
 	};
 	showFileName();
-	
+
+	// toggle categories 
+	const toggleCategories = () => {
+		const categoriesTitle  = document.querySelector('.categories-title');
+		const categoriesList  = document.querySelector('.categories-list');
+		if (categoriesTitle !== null) {
+				const collapse = new ItcCollapse(categoriesList);
+				categoriesTitle.addEventListener('click', () => {
+					collapse.toggle();
+					categoriesTitle.classList.toggle('active')
+				})
+		}
+	};
+	toggleCategories()
+
 	// tabs
 	const tabs = () => {
 		const tabNav = document.querySelector(".tab-nav");
@@ -605,60 +671,8 @@ document.addEventListener("DOMContentLoaded", () => {
 	tabs();
 
 	// dropdown
-
 	const dropdown = () => {
-		class ItcCollapse {
-			constructor(target, duration = 350) {
-				this._target = target;
-				this._duration = duration;
-			}
-			show() {
-				const el = this._target;
-				if (el.classList.contains("collapsing") || el.classList.contains("collapse_show")) {
-					return;
-				}
-				el.classList.remove("collapse");
-				const height = el.offsetHeight;
-				el.style.height = 0;
-				el.style.overflow = "hidden";
-				el.style.transition = `height ${this._duration}ms ease`;
-				el.classList.add("collapsing");
-				el.offsetHeight;
-				el.style.height = `${height}px`;
-				window.setTimeout(() => {
-					el.classList.remove("collapsing");
-					el.classList.add("collapse");
-					el.classList.add("collapse_show");
-					el.style.height = "";
-					el.style.transition = "";
-					el.style.overflow = "";
-				}, this._duration);
-			}
-			hide() {
-				const el = this._target;
-				if (el.classList.contains("collapsing") || !el.classList.contains("collapse_show")) {
-					return;
-				}
-				el.style.height = `${el.offsetHeight}px`;
-				el.offsetHeight;
-				el.style.height = 0;
-				el.style.overflow = "hidden";
-				el.style.transition = `height ${this._duration}ms ease`;
-				el.classList.remove("collapse");
-				el.classList.remove("collapse_show");
-				el.classList.add("collapsing");
-				window.setTimeout(() => {
-					el.classList.remove("collapsing");
-					el.classList.add("collapse");
-					el.style.height = "";
-					el.style.transition = "";
-					el.style.overflow = "";
-				}, this._duration);
-			}
-			toggle() {
-				this._target.classList.contains("collapse_show") ? this.hide() : this.show();
-			}
-		}
+
 
 		const dropdownSection = document.querySelector(".dropdown");
 
